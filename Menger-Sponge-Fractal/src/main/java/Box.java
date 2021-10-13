a0e2ed3dad3a9f33db217d336c39bfe7856a20ab
leo13200006 <laukikchavan101@gmail.com>
2021-10-13 13:07:59 +0530

    Menger Sponge Fractal

--- Menger-Sponge-Fractal/src/main/java/Box.java
+++ Menger-Sponge-Fractal/src/main/java/Box.java
@@ -1,1 +1,45 @@
+import processing.core.PApplet;
+import processing.core.PVector;
 
+import java.awt.*;
+import java.util.ArrayList;
+
+import static processing.core.PApplet.abs;
+
+public class Box {
+	PVector pos;
+	float r;
+	
+	public Box(float x, float y, float z, float r_) {
+		pos = new PVector(x, y, z);
+		r = r_;
+	}
+	
+	public void show(PApplet pApplet) {
+		pApplet.pushMatrix();
+		pApplet.translate(pos.x, pos.y, pos.z);
+		pApplet.fill(new Color(240, 200, 130).getRGB());
+		pApplet.strokeWeight(0);
+		pApplet.box(r);
+		pApplet.popMatrix();
+	}
+	
+	public ArrayList<Box> generate() {
+		ArrayList<Box> boxes = new ArrayList<>();
+		for (int x=-1; x < 2; x ++) {
+			for (int y=-1; y < 2; y ++) {
+				for (int z=-1; z < 2; z ++) {
+					int sum = abs(x) + abs(y) + abs(z);
+					float newR = r/3;
+					if (sum > 1) {
+						Box b = new Box(pos.x + x * newR,
+								pos.y + y * newR,
+								pos.z + z * newR, newR);
+						boxes.add(b);
+					}
+				}
+			}
+		}
+		return boxes;
+	}
+}
--- Menger-Sponge-Fractal/src/main/java/Main.java
+++ Menger-Sponge-Fractal/src/main/java/Main.java
@@ -1,48 +1,7 @@
 import processing.core.PApplet;
-import processing.core.PVector;
-
-import java.awt.*;
 import java.util.ArrayList;
 
 public class Main extends PApplet {
-	public class Box {
-		PVector pos;
-		float r;
-		
-		public Box(float x, float y, float z, float r_) {
-			pos = new PVector(x, y, z);
-			r = r_;
-		}
-		
-		public void show() {
-			pushMatrix();
-			translate(pos.x, pos.y, pos.z);
-			fill(new Color(240, 200, 130).getRGB());
-			strokeWeight(0);
-			box(r);
-			popMatrix();
-		}
-		
-		public ArrayList<Box> generate() {
-			ArrayList<Box> boxes = new ArrayList<>();
-			for (int x=-1; x < 2; x ++) {
-				for (int y=-1; y < 2; y ++) {
-					for (int z=-1; z < 2; z ++) {
-						int sum = abs(x) + abs(y) + abs(z);
-						float newR = r/3;
-						if (sum > 1) {
-							Box b = new Box(pos.x + x * newR,
-									pos.y + y * newR,
-									pos.z + z * newR, newR);
-							boxes.add(b);
-						}
-					}
-				}
-			}
-			return boxes;
-		}
-	}
-	
 	float a = 0;
 	ArrayList<Box> sponge;
 	@Override
@@ -62,7 +21,7 @@ public class Main extends PApplet {
 		rotateY(a*0.4f);
 		rotateZ(a*0.2f);
 		for (Box b : sponge)
-			b.show();
+			b.show(this);
 		
 		a += 0.01;
 	}
